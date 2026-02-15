567b29047faca8c1b51d10a92bce12a515df10be
Victor <90843661+Faugnell@users.noreply.github.com>
2026-02-15 10:57:41 +0100

    fix: add missing newline at end of tsconfig.json

--- app/app.vue
+++ app/app.vue
@@ -1,12 +1,10 @@
 <script setup lang="ts">
-const appConfig = useAppConfig();
 </script>
 
 <template>
-    <UApp :toaster="appConfig.toaster">
-        <NuxtRouteAnnouncer />
-        <NuxtLayout>
-            <NuxtPage />
-        </NuxtLayout>
-    </UApp>
+  <UApp>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </UApp>
 </template>
